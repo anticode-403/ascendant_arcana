711798c6e55704572942f1f8ba2d7c253d978fb4
Riley Rivera <rileyanticode@gmail.com>
2026-02-16 22:19:45 -0800

    Remove level cost from Anvils

--- src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
@@ -1,11 +1,13 @@
 package me.anticode.ascendant_arcana.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.anticode.ascendant_arcana.init.AArcanaItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreenHandler.class)
@@ -14,5 +16,15 @@ public abstract class AnvilScreenHandlerMixin {
     private boolean applyRestorineRepairs(Item instance, ItemStack stack, ItemStack ingredient) {
         if (ingredient.isOf(AArcanaItems.RESTORINE)) return true;
         return instance.canRepair(stack, ingredient);
+    }
+
+    @ModifyArg(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;set(I)V"))
+    private int anvilDoesNotCostLevels(int value) {
+        return 0;
+    }
+
+    @ModifyReturnValue(method = "canTakeOutput", at = @At("RETURN"))
+    private boolean canAlwaysTakeOutput(boolean value) {
+        return true;
     }
 }
