f1b88929cbdd0746175af604b34a80e25f314e32
Riley Rivera <rileyanticode@gmail.com>
2026-03-09 21:21:57 -0700

    Remove unused import

--- src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreenHandler.class)
