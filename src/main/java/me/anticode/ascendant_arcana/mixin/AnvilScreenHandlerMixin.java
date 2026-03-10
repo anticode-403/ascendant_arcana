d0f66eb5116f6bac6960d059df558f5e3624812f
Riley Rivera <rileyanticode@gmail.com>
2026-03-09 21:19:58 -0700

    Repair material repairs 50% instead of 25, Restorine repairs 12.5%

--- src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/me/anticode/ascendant_arcana/mixin/AnvilScreenHandlerMixin.java
@@ -1,6 +1,7 @@
 package me.anticode.ascendant_arcana.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.anticode.ascendant_arcana.init.AArcanaItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,6 +9,7 @@ import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreenHandler.class)
@@ -16,6 +18,12 @@ public abstract class AnvilScreenHandlerMixin {
     private boolean applyRestorineRepairs(Item instance, ItemStack stack, ItemStack ingredient) {
         if (ingredient.isOf(AArcanaItems.RESTORINE)) return true;
         return instance.canRepair(stack, ingredient);
+    }
+
+    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I"))
+    private int restorineRepairsPartially(int a, int b, @Local(ordinal = 2) ItemStack itemStack3) {
+        if (!itemStack3.isOf(AArcanaItems.RESTORINE)) return Math.min(a, b*2); // Base material repairs 50% instead of 25%
+        return Math.min(a, b/2); // Restorine repairs 12.5%
     }
 
     @ModifyArg(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;set(I)V"))
