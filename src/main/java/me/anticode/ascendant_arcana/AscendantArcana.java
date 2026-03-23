5348aa46e9ba2a576ae2a26ca5592f67e0506554
Riley Rivera <rileyanticode@gmail.com>
2026-03-22 21:42:45 -0700

    Config basics.

--- src/main/java/me/anticode/ascendant_arcana/AscendantArcana.java
+++ src/main/java/me/anticode/ascendant_arcana/AscendantArcana.java
@@ -1,6 +1,11 @@
 package me.anticode.ascendant_arcana;
 
+import me.anticode.ascendant_arcana.config.AArcanaServerConfig;
+import me.anticode.ascendant_arcana.config.AArcanaServerConfigWrapper;
 import me.anticode.ascendant_arcana.init.*;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +14,7 @@ public class AscendantArcana implements ModInitializer {
 
     public static String modID = "ascendant_arcana";
     public static final Logger LOGGER = LoggerFactory.getLogger(modID);
+    public static AArcanaServerConfig config;
 
     @Override
     public void onInitialize() {
@@ -17,5 +23,8 @@ public class AscendantArcana implements ModInitializer {
         AArcanaBlocks.initialize();
         AArcanaRecipes.initialize();
         AArcanaAttributes.initialize();
+
+        AutoConfig.register(AArcanaServerConfigWrapper.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new));
+        config = AutoConfig.getConfigHolder(AArcanaServerConfigWrapper.class).getConfig().server;
     }
 }
---
+++ src/main/java/me/anticode/ascendant_arcana/config/AArcanaServerConfig.java
@@ -0,0 +1,11 @@
+package me.anticode.ascendant_arcana.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "server")
+public class AArcanaServerConfig implements ConfigData {
+    @Comment("Multiplies the base enchantment capacity and all gains by this value. 1.0 is recommended.")
+    public double capacity_multiplier = 1.0D;
+}
