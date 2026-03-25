108911fffd4a4164b811e047dffc3cb64a9761fa
Riley Rivera <rileyanticode@gmail.com>
2026-03-25 00:24:32 -0700

    Removed Gale

--- src/main/java/me/anticode/ascendant_arcana/init/AArcanaEnchantments.java
+++ src/main/java/me/anticode/ascendant_arcana/init/AArcanaEnchantments.java
@@ -11,7 +11,6 @@ public class AArcanaEnchantments {
     public static Enchantment AMBUSH = register(new Ambush(), "ambush");
     public static Enchantment ARCHERS_GAMBIT = register(new ArchersGambit(), "archers_gambit");
     public static Enchantment EVOKERS_WRATH = register(new EvokersWrath(), "evokers_wrath");
-    public static Enchantment GALE = register(new Gale(), "gale");
     public static Enchantment SMELTING = register(new Smelting(), "smelting");
     public static Enchantment STRAFE = register(new Strafe(), "strafe");
 
