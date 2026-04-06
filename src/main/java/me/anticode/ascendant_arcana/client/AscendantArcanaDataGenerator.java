e8771aebffafea6e8591554cb59db484cadb3430
Riley Rivera <rileyanticode@gmail.com>
2026-04-06 12:19:09 -0700

    Rewrote no_item_body and no_selection_body

--- src/main/java/me/anticode/ascendant_arcana/client/AscendantArcanaDataGenerator.java
+++ src/main/java/me/anticode/ascendant_arcana/client/AscendantArcanaDataGenerator.java
@@ -260,8 +260,8 @@ public class AscendantArcanaDataGenerator implements DataGeneratorEntrypoint {
             translationBuilder.add("gui.enchanting.low_level", "This Enchanting Table does not have enough power to decrypt this enchantment.");
             translationBuilder.add("gui.enchanting.treasure", "This enchantment is too exotic to decrypt without a copy nearby.");
             translationBuilder.add("gui.enchanting.no_item_title", "Ascendant Arcana");
-            translationBuilder.add("gui.enchanting.no_item_body", "Enchanting has been completely overhauled. Each item has an enchantment capacity which can be increased here by pressing the capacity button to the left of the item.");
-            translationBuilder.add("gui.enchanting.no_selection_body", "Select an enchantment and place the costs below. Click the button to the left of your item to increase its capacity.");
+            translationBuilder.add("gui.enchanting.no_item_body", "Enchanting has been completely overhauled. Each item has an enchantment capacity which cannot be exceeded and each enchantment requires various ingredients.");
+            translationBuilder.add("gui.enchanting.no_selection_body", "Select an enchantment and place the ingredients below. Capacity can be increased with relics.");
             translationBuilder.add("gui.enchanting.item_cost", "x%1$s %2$s");
             translationBuilder.add("gui.enchanting.enchant", "Enchant!");
             // Status Effects
