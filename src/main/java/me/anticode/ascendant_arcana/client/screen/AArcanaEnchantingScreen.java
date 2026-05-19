66a79be5069713e3760c32275e18e4f6c708e0ec
Riley Rivera <rileyanticode@gmail.com>
2026-05-18 18:27:17 -0700

    Removed old, unnecessary, janky workaround

--- src/main/java/me/anticode/ascendant_arcana/client/screen/AArcanaEnchantingScreen.java
+++ src/main/java/me/anticode/ascendant_arcana/client/screen/AArcanaEnchantingScreen.java
@@ -283,7 +283,7 @@ public class AArcanaEnchantingScreen extends HandledScreen<AArcanaEnchantingScre
         boolean locked = false;
         int power = getScreenHandler().enchantmentPower[0];
         int requiredPower = AArcanaEnchantmentHelper.getRequiredEnchantmentPower(recipe.enchantment);
-        if (power < requiredPower - 1) locked = true;
+        if (power < requiredPower) locked = true;
         if (recipe.enchantment.isTreasure() && !getScreenHandler().unlockedTreasures.contains(recipe.enchantment)) locked = true;
         EnchantmentTile tile = new EnchantmentTile(recipe, buttonX, buttonY, i, locked);
         enchantments.add(tile);
